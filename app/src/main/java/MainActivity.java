23984afd547c432ba0229963ada929b55e3324fe
danny18903 <dannyjohansi17@gmail.com>
2026-09-13 13:33:01 -0600

    Add overlay permission request to prevent crash

--- app/src/main/java/MainActivity.java
+++ app/src/main/java/MainActivity.java
@@ -1,23 +1,42 @@
 package com.example.modmenu;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.provider.Settings;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    
-    static { 
+
+    static {
         System.loadLibrary("modmenu");
     }
-    @Override 
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        TextView tv = new TextView(this);
-        tv.setText(stringFromJNI());
-        setContentView(tv);
-
+        // Verificar el permiso de ventana flotante para Android 6.0 en adelante
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
+            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                    Uri.parse("package:" + getPackageName()));
+            startActivityForResult(intent, 123);
+        } else {
+            // Si ya tiene el permiso, minimiza o cierra la actividad principal de forma limpia
+            finish();
+        }
     }
-    
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 123) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && Settings.canDrawOverlays(this)) {
+                finish();
+            }
+        }
+    }
+
     public native String stringFromJNI();
 }
